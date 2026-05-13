20c3aa255b744e0b9d6fb40859db2234d954604c
Aaqil <aaqil6044@gmail.com>
2026-05-13 21:36:00 +0530

    render setup

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/jobportal");
+mongoose.connect(process.env.MONGO_URL);
 
 // Job Schema
 const Job = mongoose.model("Job", {
